cbf99319eaa9c5fb5ed80bebdea4c94d3bcdb5e9
Anup Jaiswal <150141325+anupjais@users.noreply.github.com>
2024-03-08 20:45:28 +0530

    My WebTech basics

---
+++ Piterest/DefaultValues.java
@@ -0,0 +1,23 @@
+class DefaultValues
+{
+	static byte a;
+	static short b;
+	static int c;
+	static long d;
+	static float e;
+	static double f;
+	static char g;
+	static boolean h;
+	static String i;
+	public static void main(String[] args) {
+		System.out.println("Byte : "+a);
+		System.out.println("Short : "+b);
+		System.out.println("Int : "+c);
+		System.out.println("Long : "+d);
+		System.out.println("Float : "+e);
+		System.out.println("Double : "+f);
+		System.out.println("Char : "+(g)+0);
+		System.out.println("Boolean : "+h);
+		System.out.println("String : "+i);
+	}
+}
